8417e0ca3634e3caedc07a88d0208f7c6d39586c
Sh3rlock7K <kevindebonis7@hotmail.com>
2024-05-18 01:51:37 +0200

    Initial configuration

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-black p-2 text-6xl text-white text-center">My Portfolio</h1>
+    </>
+  )
+}
+
+export default App
