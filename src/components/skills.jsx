f46123941e8656e556a64a480bbdb0956c3dc337
Sh3rlock7K <kevindebonis7@hotmail.com>
2024-08-06 14:23:05 +0200

    general debugging

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import html5img from '/HTML5.png';
 
 
 export const Skills = () => {
@@ -12,7 +11,7 @@ export const Skills = () => {
         <div className='flex flex-row lg:flex-1 flex-wrap justify-between p-10 text-lg  font-bold bg-black bg-opacity-60'>
             <div className='text-center md:flex-20 lg:flex-none'>
                 <p>HTML 5</p>
-                <img className='h-24 p-3 m-auto' src={html5img} alt="" />
+                <img className='h-24 p-3 m-auto' src="/HTML5.png" alt="" />
             </div>
             <div className='text-center md:flex-20 lg:flex-none'>
                 <p>CSS3</p>
